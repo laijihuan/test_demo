c2027ff3658d18c0dfa06fcb94a4856a27774e79
laijihuan <laijihuan@nawang.cn>
2017-06-01 16:29:28 +0800

    添加工程

---
+++ TestDuiLib/TestDuiLib/UIlib.cpp
@@ -0,0 +1,16 @@
+
+#include "StdAfx.h"
+#include "UIlib.h"
+
+BOOL APIENTRY DllMain( HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved )
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
